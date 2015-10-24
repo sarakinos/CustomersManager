355619cf5f9785b6ba9b9da57500846534a12dc4
Bampis Sykovaridis <bampis.s@gmail.com>
2015-10-24 21:43:15 +0300

    Generated Customer entity and added the index functionality to the CustomerController

--- src/AppBundle/Controller/CustomersController.php
+++ src/AppBundle/Controller/CustomersController.php
@@ -3,11 +3,17 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Customer;
 
 class CustomersController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('customers_manager/customers.html.twig', array('title' => 'Customers Managmentn'));
+        $customers = $this->getDoctrine()->getRepository("AppBundle:Customer")->findAll();
+
+        return $this->render('customers_manager/customers.html.twig', array(
+            'title' => 'Customers Managment',
+            'customers'=>$customers
+        ));
     }
 }
