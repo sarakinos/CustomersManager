fb93bfd3615bd182448504708fcc600af16335ba
Bampis Sykovaridis <bampis.s@gmail.com>
2015-10-25 12:14:33 +0200

    Added customer register feature. Need validation settings

--- src/AppBundle/Controller/CustomersController.php
+++ src/AppBundle/Controller/CustomersController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\Customer;
+use AppBundle\Form\Type\CustomerType;
+use Symfony\Component\HttpFoundation\Request;
 
 class CustomersController extends Controller
 {
@@ -11,9 +13,42 @@ class CustomersController extends Controller
     {
         $customers = $this->getDoctrine()->getRepository("AppBundle:Customer")->findAll();
 
-        return $this->render('customers_manager/customers.html.twig', array(
-            'title' => 'Customers Managment',
+        return $this->render('customers_manager/customers/customers_index.html.twig', array(
+            'title' => 'Customers Managment - Index',
             'customers'=>$customers
         ));
+    }
+    public function registerAction()
+    {
+        $customer = new Customer();
+        $registerForm = $this->createForm(new CustomerType(),$customer,array(
+            'action' => $this->generateUrl('customer_manager_customers_add'),
+            'method' => 'POST',
+            ));
+
+        return $this->render('customers_manager/customers/customers_add.html.twig', array(
+            'title' => 'Customers Managment - Add',
+            'registerForm' => $registerForm->createView()
+        ));
+    }
+
+    public function addAction(Request $request)
+    {
+        $customer = new Customer();
+        $registerForm = $this->createForm(new CustomerType(),$customer,array(
+            'action' => $this->generateUrl('customer_manager_customers_add'),
+            'method' => 'POST',
+        ));
+        $registerForm->handleRequest($request);
+
+        if(!$registerForm->isValid()){
+            return $this->redirectToRoute("customer_manager_customers_register");
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($customer);
+        $em->flush();
+
+        return $this->redirectToRoute("customer_manager_customers_index");
     }
 }
---
+++ src/AppBundle/Form/Type/CustomerType.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 24/10/2015
+ * Time: 10:00 μμ
+ */
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CustomerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname','text')
+            ->add('surname','text')
+            ->add('city','text')
+            ->add('country','text')
+            ->add('address','text')
+            ->add('phone','text')
+            ->add('email','text')
+            ->add('save', 'submit')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Customer'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'customer';
+    }
+}
