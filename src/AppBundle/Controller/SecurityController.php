2e48a6767094ac8e03db886d894f1c976dba0391
Bampis Sykovaridis <bampis.s@gmail.com>
2015-10-31 16:11:00 +0200

    First atemp adding a login system adding the Security controller and the security config and routing, also some css and html stractural changes

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            'customers_manager/security/login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+    }
+    public function loginCheckAction()
+    {
+
+    }
+    public function registerAction()
+    {
+        return $this->render(
+            'customers_manager/security/register.html.twig'
+        );
+    }
+}
